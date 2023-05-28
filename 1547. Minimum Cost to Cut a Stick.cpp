ac825a15280687391d6d8e91a0b6fa9e23c5a1d8
Kunal Sabharwal <ksabharwal77@gmail.com>
2023-05-28 11:37:53 +0530

    Create 1547. Minimum Cost to Cut a Stick.cpp

---
+++ 1547. Minimum Cost to Cut a Stick.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+
+        int c = cuts.size();
+        vector<vector<int>> dp(c, vector<int>(c, 0));
+
+        for (int len = 2; len < c; len++) {
+            for (int i = 0; i < c - len; i++) {
+                int j = i + len;
+                dp[i][j] = INT_MAX;
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + cuts[j] - cuts[i]);
+                }
+            }
+        }
+
+        return dp[0][c - 1];
+    }
+};
