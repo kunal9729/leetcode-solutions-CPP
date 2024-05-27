e8bf9c5d0e6383ce7e6441df0ac88a57209a7fce
Kunal Sabharwal <ksabharwal77@gmail.com>
2024-05-27 23:38:47 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (19.80%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+    sort(nums.begin(), nums.end());  // O(NlogN)
+    const int N = nums.size();
+    if (nums[0] >= N) return N;
+    int l = 1, r = N - 1;
+    while (l <= r) { // O(LogN) * O(1)
+        int m = l + (r - l) / 2;
+        if (nums[m] >= (N - m) && nums[m - 1] < (N - m)) return (N - m);
+        else if (nums[m] >= (N - m)) r = m - 1;
+        else l = m + 1;
+    }
+    return -1;
+    }
+    };
+ 
